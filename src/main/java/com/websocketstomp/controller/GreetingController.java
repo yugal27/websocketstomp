f595dd01a9ca869998835be2ca1b35c4eee31825
yugal nankani <nankani.yugal@gmail.com>
2019-08-02 11:07:50 +0200

    Initial import

---
+++ src/main/java/com/websocketstomp/controller/GreetingController.java
@@ -0,0 +1,22 @@
+package com.websocketstomp.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+import com.websocketstomp.models.Greeting;
+import com.websocketstomp.models.HelloMessage;
+
+@Controller
+public class GreetingController {
+
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+
+}
